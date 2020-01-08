354c76b1b8d41e9a6ece58dda5cd3e31cdee3622
Ricardo <568082704@qq.com>
2020-01-08 11:06:12 +0800

    准备

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Home',
+  data () {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+/*@import url()*/
+  
+</style>
