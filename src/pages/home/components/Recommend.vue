c6ba8f6e8cbb5bac39f0b107293c7147dd061992
Ricardo <568082704@qq.com>
2020-01-12 17:20:25 +0800

    save

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <div class="recommend-title">热销推荐</div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img :src="item.imgUrl" alt="" class="item-img">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -12,7 +22,26 @@ export default {
   },
   data () {
     return {
-
+      recommendList:[
+        {
+          id:'0001',
+          imgUrl:'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_200x200_4ab5da75.jpg',
+          title:'大连圣亚海洋世界',
+          desc:'浪漫大连首站，浪漫的海洋主题公园'
+        },
+        {
+          id:'0002',
+          imgUrl:'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_200x200_4ab5da75.jpg',
+          title:'大连圣亚海洋世界',
+          desc:'浪漫大连首站，浪漫的海洋主题公园'
+        },
+        {
+          id:'0003',
+          imgUrl:'http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_200x200_4ab5da75.jpg',
+          title:'大连圣亚海洋世界',
+          desc:'浪漫大连首站，浪漫的海洋主题公园'
+        },
+      ]
     }
   },
   methods: {
@@ -22,9 +51,39 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  .recommend-title
-    margin-top .2rem
+  @import '~styles/mixins.styl';
+  .title
+    margin-top .2rem  
     line-height .8rem
     background #eee
     text-indent .2rem
+  .item
+    overflow hidden
+    display flex
+    height 1.9rem  
+    .item-img
+      width 1.7rem 
+      padding .1rem
+      height 1.7rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        ellipsis()
+        line-height .54rem
+        font-size .32rem
+      .item-desc
+        ellipsis()
+        line-height .4rem
+        color #ccc
+      .item-button
+        line-height .44rem
+        margin-top .16rem
+        background #ff9300
+        padding 0 .2rem
+        border-radius .06rem
+        color #fff
+
+
 </style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img :src="item.imgUrl" alt="" class="item-img">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HomeWeekend',
+  components: {
+
+  },
+  data () {
+    return {
+      weekendList:[
+        {
+          id:'0001',
+          imgUrl:'http://img1.qunarzz.com/sight/source/1812/b4/e9d7f043c4fadf.jpg_r_640x214_713df0cf.jpg',
+          title:'大连圣亚海洋世界',
+          desc:'浪漫大连首站，浪漫的海洋主题公园'
+        },
+        {
+          id:'0002',
+          imgUrl:'http://img1.qunarzz.com/sight/source/1812/b4/e9d7f043c4fadf.jpg_r_640x214_713df0cf.jpg',
+          title:'大连圣亚海洋世界',
+          desc:'浪漫大连首站，浪漫的海洋主题公园'
+        },
+        {
+          id:'0003',
+          imgUrl:'http://img1.qunarzz.com/sight/source/1812/b4/e9d7f043c4fadf.jpg_r_640x214_713df0cf.jpg',
+          title:'大连圣亚海洋世界',
+          desc:'浪漫大连首站，浪漫的海洋主题公园'
+        },
+      ]
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl';
+  .title
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    height 37.8vw
+    .item-img
+      width 100%
+  .item-info
+    padding  .14rem .2rem .2rem .2rem 
+    .item-title
+      ellipsis()
+      line-height .54rem
+      font-size .32rem
+    .item-desc
+      ellipsis()
+      line-height .4rem
+      color #ccc
+
+
+</style>
