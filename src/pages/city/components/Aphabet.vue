7a15c47550b96e73d6488edc8fd5749e6eb20c07
Ricardo <568082704@qq.com>
2020-01-13 20:53:30 +0800

    finish city css

---
+++ src/pages/city/components/Aphabet.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name:'CityAphabet',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    display flex
+    flex-direction column
+    justify-content center
+    align-items center
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    width .4rem
+    .item 
+      line-height .4rem
+      color $bgColor
+    
+
+
+
+
+
+
+
+</style>
