882ac6b639018cbe991664a1afba770ffaf3d687
Ricardo <568082704@qq.com>
2020-01-17 20:54:10 +0800

    save

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="wrapper">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'CommonGallary',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    z-index 10
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background #000
+    .wrapper  
+      
+</style>
--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,14 +2,18 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item of list" :key="item.id">
+      <router-link 
+        class="item border-bottom" v-for="item of list" :key="item.id"
+        tag="li"
+        :to="'/detail/'+item.id"
+      >
         <img :src="item.imgUrl" alt="" class="item-img">
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
