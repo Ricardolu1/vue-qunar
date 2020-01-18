045a834e8d312a6f68fcc1147f453d35cb8c93cb
Ricardo <568082704@qq.com>
2020-01-18 20:50:29 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,12 @@
     <keep-alive exclude="Detail">
       <router-view/>
     </keep-alive>
-    <home></home>
   </div>
 </template>
 
 <script>
-import Home from '@/pages/home/Home'
 export default {
   name: 'App',
-  components:{
-    Home
-  }
 }
 </script>
 
